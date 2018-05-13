782583b5b046ad820e20f40a106c50c1d360b6bf
Bastien Tomasetti <36277197+Blasterino@users.noreply.github.com>
2018-05-13 19:10:06 +0200

    nul

--- src/FenetreJeu.java
+++ src/FenetreJeu.java
@@ -5,7 +5,7 @@ public class FenetreJeu extends JFrame {
     Plateau plateau;
     JButton[] listeColonnes;
     JPanel[][] grille;
-    JButton bSauterLigne;
+    JButton bSauterLigne, bQuitter;
 
     public FenetreJeu(Plateau p){
         this.plateau=p;
@@ -13,15 +13,44 @@ public class FenetreJeu extends JFrame {
         ajouterWidget();
         pack();
         setTitle("Le Jeu");
+        this.plateau = p;
+        initAttribut();
+        creerVue();
+        pack();
         setVisible(true);
     }
 
     public void initAttribut(){
+<<<<<<< HEAD
         JPanel pFenetre = new JPanel();
         setContentPane(pFenetre);
     }
 
     public void ajouterWidget(){
+=======
+        for (int i =0; i<plateau.getPlateau().length;i++){
+            listeColonnes[i] = new JButton(String.valueOf(i));
+        }
 
+        for(int i = 0; i<plateau.getPlateau().length;i++){
+            for (int j =0; j<plateau.getPlateau()[0].length;j++){
+                grille[i][j] = new JPanel();
+            }
+        }
+        bSauterLigne = new JButton("Activer la puissance 4");
+        bQuitter = new JButton("Quitte");
+    }
+
+    public void creerVue(){
+        return;
+    }
+>>>>>>> 7575f5265083f714dff5d124bf67319ca26a4385
+
+    public void setControlButton(ControlButtonJeu cb){
+        for (JButton butt : listeColonnes){
+            butt.addActionListener(cb);
+        }
+        bSauterLigne.addActionListener(cb);
+        bQuitter.addActionListener(cb);
     }
 }
