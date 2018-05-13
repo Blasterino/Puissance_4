7575f5265083f714dff5d124bf67319ca26a4385
Nathan_Bodet <bbodetnathan@gmail.com>
2018-05-13 19:02:13 +0200

    githubcnul

--- src/FenetreJeu.java
+++ src/FenetreJeu.java
@@ -7,7 +7,37 @@ public class FenetreJeu extends JFrame {
     JPanel[][] grille;
     JButton bSauterLigne, bQuitter;
 
-    public FenetreJeu(){
+    public FenetreJeu(Plateau p){
+        this.plateau = p;
+        initAttribut();
+        creerVue();
+        pack();
+        setVisible(true);
+    }
 
+    public void initAttribut(){
+        for (int i =0; i<plateau.getPlateau().length;i++){
+            listeColonnes[i] = new JButton(String.valueOf(i));
+        }
+
+        for(int i = 0; i<plateau.getPlateau().length;i++){
+            for (int j =0; j<plateau.getPlateau()[0].length;j++){
+                grille[i][j] = new JPanel();
+            }
+        }
+        bSauterLigne = new JButton("Activer la puissance 4");
+        bQuitter = new JButton("Quitte");
+    }
+
+    public void creerVue(){
+        return;
+    }
+
+    public void setControlButton(ControlButtonJeu cb){
+        for (JButton butt : listeColonnes){
+            butt.addActionListener(cb);
+        }
+        bSauterLigne.addActionListener(cb);
+        bQuitter.addActionListener(cb);
     }
 }
